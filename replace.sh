f2b388351525ee245c24ffd2539714e874b92c44
srcwap7 <arpang@iitbhilai.ac.in>
2026-01-15 19:56:46 +0530

    removed unneccessary files

--- replace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Replace URLs like http://localhost:2000
-# with http://localhost:2000
-
-find . -type f -exec sed -i.bak \
-  's|https://swapkard\.onrender\.com/[A-Za-z0-9_-]\+|http://localhost:2000|g' {} +
-
-echo "Replacement done. Backup files created with .bak extension."
