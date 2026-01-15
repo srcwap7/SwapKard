7e1faf4599e186ae12ab24c4fca258f51b4b99b6
srcwap7 <arpang@iitbhilai.ac.in>
2026-01-15 16:10:53 +0530

    Removed redundant APIs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const {sendEmailOtp,registerUser,loginUser,logoutUser,forgotPass,resetPassword,loadUser,verifyEmail,uploadProfilePic,registerUserMobile } = require("../controller/userController");
+const {sendEmailOtp,loginUser,logoutUser,forgotPass,resetPassword,loadUser,verifyEmail,uploadProfilePic,registerUserMobile } = require("../controller/userController");
 const {forgotPasswordMobile, resetPasswordMobile, loginUserMobileSignedUp} = require("../controller/userController");
 const {loginUserMobile} = require("../controller/userController");
 const { isAuthenticated } = require("../middleware/auth");
@@ -7,13 +7,10 @@ const {verifyToken} = require("../middleware/isTokenValid");
 
 const router=express.Router();
 
-router.route("/register").post(registerUser);
+
 router.route("/verify-email").post(verifyEmail);
-router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
 router.route("/forgotPass").post(forgotPass)
 router.route("/resetPass/:token").post(resetPassword)
-router.route("/me").get(isAuthenticated, loadUser);
 router.route("/sendOtp").post(sendEmailOtp);
 router.route("/uploadProfilePic").post(verifyToken,uploadProfilePic);
 router.route("/details").post(verifyToken,registerUserMobile);
